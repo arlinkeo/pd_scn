b0e7ab91095b38a226fc13f7f0814ee72dfaedf4
arlinkeo <arlinkeo@gmail.com>
2019-05-17 18:07:27 +0200

    Merged network info

--- samples_in_networks.R
+++ samples_in_networks.R
@@ -4,8 +4,6 @@ options(stringsAsFactors = FALSE)
 
 donorNames <- c("donor9861", "donor10021", "donor12876", "donor14380", "donor15496", "donor15697")
 names(donorNames) <- donorNames
-samples_networkC <- read.delim("Results_Oleh/Network_C.txt", skip = 1)
-samples_networkD <- read.delim("Results_Oleh/Network_D.txt", skip = 1)
 
 # Function to read mapping of AHBA samples to network
 read.network <- function(nw, d){
@@ -22,17 +20,21 @@ read.network <- function(nw, d){
   l
 }
 
+# Read and merge info of both networks C and D
+samples_networkC <- read.delim("Results_Oleh/Network_C.txt", skip = 1)
+samples_networkD <- read.delim("Results_Oleh/Network_D.txt", skip = 1)
 samples_C <- read.network(samples_networkC, donorNames)
 samples_D <- read.network(samples_networkD, donorNames)
-saveRDS(samples_C, file = "resources/samples_C.rds")
-saveRDS(samples_D, file = "resources/samples_D.rds")
+sample_info <- lapply(donorNames, function(d){
+  cbind(samples_C[[d]][, c(1:3)],
+    'Network_C' = samples_C[[d]]$Inside.y.n, 'Network_D' = samples_D[[d]]$Inside.y.n)
+})
+saveRDS(sample_info, file = "resources/sample_info.rds")
 
 # Number of samples within network
 df <- data.frame(
   'Donors' = gsub("donor", "Donor ", donorNames),
   'All_samples' = sapply(samples_C, nrow),
-  'Network_C' = sapply(samples_C, function(tab){sum(tab$Inside.y.n)}),
-  'Network_D' = sapply(samples_D, function(tab){sum(tab$Inside.y.n)})
+  t(sapply(sample_info, function(t){ apply(t[, c(4,5)], 2, sum)}))
 )
 write.table(df, "number_of_samples.txt", row.names = FALSE, quote = FALSE, sep = "\t")
-
