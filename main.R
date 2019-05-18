2f0169d7bfd47cd3f634be1c1869a295e59bbb19
arlinkeo <arlinkeo@gmail.com>
2019-05-18 04:24:33 +0200

    .

--- main.R
+++ main.R
@@ -1,8 +1,10 @@
+setwd("C:/Users/dkeo/surfdrive/pd_imaging_scn")
+
 source("samples_in_networks.R")
 
 
 Sys.time()
-source("ttest")
+source("pd_scn/ttest.R")
 Sys.time()
-source("Rdavid.R")
+source("pd_scn/Rdavid.R")
 Sys.time()
