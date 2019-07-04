e664a507ef4ed8b84e3b6f7b8c746ccd42488563
arlinkeo <arlinkeo@gmail.com>
2019-07-04 14:37:07 +0200

    Useful directories, functions and variables

--- main.R
+++ main.R
@@ -1,4 +1,16 @@
-setwd("C:/Users/dkeo/surfdrive/pd_imaging_scn")
+setwd("C:/Users/dkeo/surfdrive/pd_imaging_scn/pd_scn")
+
+# AHBA data directory
+ahba_dir <-"C:/Users/dkeo/surfdrive/AHBA_Arlin"
+probeInfo <- read.csv(paste0(ahba_dir, "/probe_info_2018-11-18.csv"))
+
+# Source directory with functions
+fun_dir <- paste0(getwd(), "/functions")
+R.utils::sourceDirectory(fun_dir)
+
+# Useful variables
+donorNames <- c("donor9861", "donor10021", "donor12876", "donor14380", "donor15496", "donor15697")
+names(donorNames) <- donorNames
 
 source("samples_in_networks.R")
 
