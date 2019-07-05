7a7d1f275980fee2d199bb6812cf4d6a5a3d8a0b
arlinkeo <arlinkeo@gmail.com>
2019-07-05 11:23:45 +0200

    print number and write tables

--- differential_expression.R
+++ differential_expression.R
@@ -1,15 +1,9 @@
-setwd("C:/Users/dkeo/surfdrive/pd_imaging_scn")
-options(stringsAsFactors = FALSE)
+# Differential expression between C or D and remaining networks A, B, E, F, G, H, and I
+
 library(plyr)
 library(metafor)
 library(venn)
 source("../pd_braak/PD/t.test.table.R")
-
-donorNames <- c("donor9861", "donor10021", "donor12876", "donor14380", "donor15496", "donor15697")
-names(donorNames) <- donorNames
-
-brainExpr <- readRDS("../AHBA_Arlin/gene_expr.RDS")
-sample_info <- readRDS("resources/sample_info.rds")
 
 # Differential expression between network and rest of the brain (t-test)
 ttest <- lapply(donorNames, function(d){
@@ -19,15 +13,15 @@ ttest <- lapply(donorNames, function(d){
   abefghi <- apply(abefghi, 1, function(x) Reduce(bitwOr, x))
   cd <- samples[, c("Network_C", "Network_D")] # Networks affected in PD
   tab <- alply(cd, 2, function(s){
-    a <- expr[, s==1] # expression in network C or D
-    b <- expr[, abefghi==1]# expression in network ABEFGHI
+    a <- expr[, abefghi==1]# expression in network ABEFGHI
+    b <- expr[, s==1] # expression in network C or D
     t.test.table(a, b) # in compared to out
   }, .dims = TRUE) # keep names
   simplify2array(tab) # 3D array: genes x measures x networks
 })
 ttest <- simplify2array(ttest) # 4D array: genes x measures x networks x donors
-saveRDS(ttest, file = "resources/ttest.rds")
-ttest <- readRDS("resources/ttest.rds")
+saveRDS(ttest, file = "output/ttest.rds")
+# ttest <- readRDS("output/ttest.rds")
 
 # Meta-analysis of differential expression across donors
 summary_ttest <- aaply(ttest, c(1,3), function(g){ # For each Braak region pair and gene
@@ -51,39 +45,52 @@ summary_ttest <- aaply(summary_ttest, c(2,3), function(t){ # P-value corrected f
   b <- p.adjust(t[, "pvalue"], method = "BH")
   cbind(t, BH = b)
 }) # 4D-array: networks x donors x genes x measures (+BH)
-saveRDS(summary_ttest, file = "resources/summary_ttest.rds")
-summary_ttest <- readRDS("resources/summary_ttest.rds")
+saveRDS(summary_ttest, file = "output/summary_ttest.rds")
+# summary_ttest <- readRDS("output/summary_ttest.rds")
 
-# # Print number of diff. expr. genes per donor and summary
-# df <- aaply(summary_ttest, c(1,2), function(x){
-#   down <- x[which(x[, "Estimate"] < -1 & x[,"BH"] < 0.05),]
-#   up <- x[which(x[, "Estimate"] > 1 & x[,"BH"] < 0.05),]
-#   n1 <- nrow(down)
-#   n2 <- nrow(up)
-#   n1 <- ifelse(is.null(n1), 0, n1)
-#   n2 <- ifelse(is.null(n2), 0, n2)
-#   c(down = n1, up = n2)
-# })
-# df <- alply(df, 1, function(x)x)
-# df <- Reduce(cbind, df)
-# colnames(df) <- c("Downregulated in network C", "Upregulated in network C", "Downregulated in network D", "Upregulated in network D")
-# df <- cbind(Donor = gsub("donor", "Donor ", rownames(df)), df)
+# Print number of diff. expr. genes per donor and summary
+df <- aaply(summary_ttest, c(1,2), function(x){
+  down <- x[which(x[, "Estimate"] < -1 & x[,"BH"] < 0.05),]
+  up <- x[which(x[, "Estimate"] > 1 & x[,"BH"] < 0.05),]
+  n1 <- nrow(down)
+  n2 <- nrow(up)
+  n1 <- ifelse(is.null(n1), 0, n1)
+  n2 <- ifelse(is.null(n2), 0, n2)
+  c(down = n1, up = n2)
+})
+df <- alply(df, 1, function(x)x)
+df <- Reduce(cbind, df)
+colnames(df) <- c("Downregulated in network C", "Upregulated in network C", "Downregulated in network D", "Upregulated in network D")
+df <- cbind(Donor = gsub("donor", "Donor ", rownames(df)), df)
+df
 
 # Get tables of DEGs
 degs <- alply(summary_ttest[, "summary", , ], 1, function(x){
+  x <- cbind('gene'= entrezId2Name(rownames(x)), 'gene_id' = rownames(x), x)
   down <- x[which(x[, "Estimate"] < -1 & x[,"BH"] < 0.05),]
   down <- down[order(down[,"BH"]),]
   up <- x[which(x[, "Estimate"] > 1 & x[,"BH"] < 0.05),]
   up <- up[order(up[,"BH"]),]
   list(downregulated = down, upregulated = up)
 }, .dims = TRUE)
-saveRDS(degs, file = "resources/degs.rds")
+saveRDS(degs, file = "output/degs.rds")
+
+# Write table of DEGs
+lapply(names(degs), function(network){
+  direction <- degs[[network]]
+  lapply(names(direction), function(d){
+    t <- degs[[network]][[d]]
+    colnames(t) <- paste0(toupper(substr(colnames(t), 1, 1)), substring(colnames(t), 2))
+    colnames(t)[7] <- "P-value"
+    write.table(t, file = paste0("output/degs_", network,"_", d, ".txt"), 
+                sep = "\t", quote = FALSE, row.names = FALSE)
+  })
+})
 
 # Print number of diff. expr. genes
-df <- sapply(degs, function(l){
-  sapply(l, nrow)
-})
-write.table(df, file = "number_of_degs.txt", sep = "\t", quote = FALSE)
+df <- sapply(degs, function(l) sapply(l, nrow))
+rownames(df) <- paste0(toupper(substr(rownames(df), 1, 1)), substring(rownames(df), 2))
+write.table(df, file = "output/number_of_degs.txt", sep = "\t", quote = FALSE)
 
 # Overlap of DEGs between network C and D
 ll_degs <- lapply(degs, function(l) lapply(l, rownames))
