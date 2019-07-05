1276cbb625ea34678fe34ce1b6f59fcba0eb07f4
arlinkeo <arlinkeo@gmail.com>
2019-07-06 00:50:49 +0200

    clean up

--- function_enrichment_degs.R
+++ function_enrichment_degs.R
@@ -6,13 +6,11 @@ david<-DAVIDWebService$new(email="D.L.Keo@tudelft.nl",
                            url="https://david.abcc.ncifcrf.gov/webservice/services/DAVIDWebService.DAVIDWebServiceHttpSoap12Endpoint/")
 setAnnotationCategories(david, c("GOTERM_BP_ALL", "GOTERM_MF_ALL", "GOTERM_CC_ALL"))
 setTimeOut(david, 200000)
-bg_list <- rownames(brainExpr$donor9861)
-rm(brainExpr)
-bg <- addList(david, bg_list, idType = "ENTREZ_GENE_ID", listName = "AHBA background", listType = "Background")
+bg <- addList(david, ahba.genes(), idType = "ENTREZ_GENE_ID", listName = "AHBA background", listType = "Background")
 bg
-t <- 0.05 # EASE p-value threshold
 
-mkdirs("output/Functional_enrichment")
+output_dir <- "output/Functional_enrichment/"
+mkdirs(output_dir)
 
 lapply(names(degs), function(n){
   lapply(names(degs[[n]]), function(l){
@@ -21,26 +19,17 @@ lapply(names(degs), function(n){
     result <- addList(david, genes, idType = "ENTREZ_GENE_ID", listName = name, listType = "Gene")
     print(result)
     setCurrentBackgroundPosition(david, 1)
-    getFunctionalAnnotationChartFile(david, paste0("output/Functional_enrichment/", name, "_goterms.txt"), threshold=t, count=2L)
-    getClusterReportFile(david, paste0("output/Functional_enrichment/", name, "_termclusters.txt"), type = c("Term"))
+    getFunctionalAnnotationChartFile(david, paste0(output_dir, name, "_goterms.txt"), threshold=0.05, count=2L)
+    getClusterReportFile(david, paste0(output_dir, name, "_termclusters.txt"), type = c("Term"))
   })
 })
-
-#Function to read Rdavid output
-read.RdavidOutput <- function(fileName){
-  if (file.exists(fileName)){
-    terms <- read.csv(fileName, header = TRUE, sep = "\t", colClasses = "character")
-  } else {
-    NULL
-  }
-}
 
 go <- sapply(names(degs), function(n){
   sapply(names(degs[[n]]), function(l){
     name <- paste(n, l, sep = "_")
-    fName <- paste0("output/", name, "_goterms.txt")
+    fName <- paste0(output_dir, name, "_goterms.txt")
     print(fName)
-    t <- read.RdavidOutput(fName)
+    t <- read.csv(fName, header = TRUE, sep = "\t", colClasses = "character")
     rows <- t$Benjamini < 0.05
     t <- t[rows, c("Term", "Count", "Benjamini")]
     t[order(t$Benjamini), ]
@@ -51,5 +40,5 @@ go <- go[-which(sapply(go, nrow) == 0)]
 
 lapply(names(go), function(n){
   t <- go[[n]]
-  write.table(t, file = paste0("output/Functional_enrichment/BHcorrected_", n, ".txt"), row.names = FALSE, quote = FALSE, sep = "\t")
+  write.table(t, file = paste0(output_dir, "/BHcorrected_", n, ".txt"), row.names = FALSE, quote = FALSE, sep = "\t")
 })
