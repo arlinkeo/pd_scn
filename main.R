826c56077d335676b686440cb3d93a33e0019084
arlinkeo <arlinkeo@gmail.com>
2019-07-05 12:40:28 +0200

    changed file names and structures

--- main.R
+++ main.R
@@ -3,9 +3,10 @@
 setwd("C:/Users/dkeo/surfdrive/pd_imaging_scn/pd_scn")
 options(stringsAsFactors = FALSE)
 
-# AHBA data directory
+# AHBA data directory and data
 ahba_dir <-"C:/Users/dkeo/surfdrive/AHBA_Arlin"
 probeInfo <- read.csv(paste0(ahba_dir, "/probe_info_2018-11-18.csv"))
+brainExpr <- readRDS(paste0(ahba_dir, "/gene_expr.RDS"))
 
 # Source directory with functions
 fun_dir <- paste0(getwd(), "/functions")
@@ -15,11 +16,13 @@ R.utils::sourceDirectory(fun_dir)
 donorNames <- c("donor9861", "donor10021", "donor12876", "donor14380", "donor15496", "donor15697")
 names(donorNames) <- donorNames
 
+# Run scripts
 source("samples_in_networks.R")
-
-
 Sys.time()
-source("pd_scn/differential_expression.R")
+source("differential_expression.R")
 Sys.time()
-source("pd_scn/Rdavid.R")
+source("functional_enrichment_degs.R")
 Sys.time()
+source("celltype_marker_conversion.R")
+Sys.time()
+source("celltype_enrichment_degs.R")
