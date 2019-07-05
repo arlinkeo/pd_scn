1aba1d34a1eda6d7e52a90cfce48d49c19b8a83c
arlinkeo <arlinkeo@gmail.com>
2019-07-06 00:50:00 +0200

    more scripts

--- main.R
+++ main.R
@@ -7,6 +7,10 @@ options(stringsAsFactors = FALSE)
 ahba_dir <-"C:/Users/dkeo/surfdrive/AHBA_Arlin"
 probeInfo <- read.csv(paste0(ahba_dir, "/probe_info_2018-11-18.csv"))
 brainExpr <- readRDS(paste0(ahba_dir, "/gene_expr.RDS"))
+ontology <- read.csv(paste0(ahba_dir, "/Ontology.csv"))
+# sample_annot <- lapply(donorNames, function(d){ # Sample info per donor
+#   read.csv(paste0("../AHBA_Arlin/sample_info_", d, "_2018-11-18.csv"))
+# })
 
 # Source directory with functions
 fun_dir <- paste0(getwd(), "/functions/")
@@ -17,16 +21,9 @@ donorNames <- c("donor9861", "donor10021", "donor12876", "donor14380", "donor154
 names(donorNames) <- donorNames
 
 # Run scripts
-Sys.time()
 source("samples_in_networks.R")
-Sys.time()
 source("differential_expression.R")
-Sys.time()
 source("functional_enrichment_degs.R")
-Sys.time()
-
-
-
 source("celltype_marker_conversion.R")
-Sys.time()
 source("celltype_enrichment_degs.R")
+source("heatmap_celltypes.R")
