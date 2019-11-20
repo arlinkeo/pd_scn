21b5fd551e3208354a69bc61e8ffd84d88f647e4
arlinkeo <arlinkeo@gmail.com>
2019-11-20 16:38:26 +0100

    change name of microglia

--- celltype_marker_conversion.R
+++ celltype_marker_conversion.R
@@ -6,10 +6,24 @@ dir <- "../../Neuroexpresso_markers/markerGenesNCBI/All"
 filenames <- list.files(dir)
 conversion_table <- sapply(filenames, function(f){
   m <- unlist(read.table(paste0(dir, "/", f)))
-  homologene(m, inTax = 10090, outTax = 9606) # Convert mouse entrez IDs to human ortholog entrez IDs
+  t <- homologene(m, inTax = 10090, outTax = 9606) # Convert mouse entrez IDs to human ortholog entrez IDs
 }, simplify = FALSE)
+names(conversion_table)[names(conversion_table) %in% c("Microglia_activation", "Microglia_deactivation")] <- c("Activated microglia", "Deactivated microglia")
 
 # Convert to human gene entrez IDs and filter for genes present in AHBA
+markerlist <- lapply(conversion_table, function(x) {
+  l <- x[, "9606_ID"] # Human entrez IDs
+  intersect(l, ahba.genes())# Filter for genes present in AHBA
+})
+
+# Check for duplicate markers 
+all_markers <- unlist(markerlist)
+dup <- all_markers[duplicated(all_markers)]
+conversion_table <- sapply(names(conversion_table), function(x) {
+  t <- conversion_table[[x]]
+  t <- t[!(t$`9606_ID` %in% dup), ]
+  cbind(x, t)
+}, simplify = FALSE)
 markerlist <- lapply(conversion_table, function(x) {
   l <- x[, "9606_ID"] # Human entrez IDs
   intersect(l, ahba.genes())# Filter for genes present in AHBA
@@ -22,11 +36,3 @@ df_size <- cbind('Cell-type' = rownames(df_size), df_size)
 df_size <- do.call("cbind", split(df_size, c(rep(1, nrow(df_size)/2), rep(2, nrow(df_size)/2))))
 write.table(df_size, file = "output/number_of_celltype_markers.txt", sep = "\t", quote = FALSE, row.names = FALSE)
 
-# Check for duplicate markers 
-all_markers <- unlist(markerlist)
-dup <- all_markers[duplicated(all_markers)]
-conversion_table <- lapply(names(conversion_table), function(x) {
-  cbind(x, conversion_table[[x]])
-})
-conversion_table <- Reduce(rbind, conversion_table)
-conversion_table[conversion_table$`9606_ID` %in% dup, ]
